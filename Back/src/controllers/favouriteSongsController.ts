7b5b600107f21243d0fd34758a80a11566886b5c
Vicky <nadia.gonzalez@valtech.com>
2023-06-07 13:59:48 -0300

    changes

--- Back/src/controllers/favouriteSongsController.ts
+++ Back/src/controllers/favouriteSongsController.ts
@@ -32,31 +32,3 @@ export async function getFavoriteSongs(req: Request, res: Response) {
   }
 }
 
-import axios from 'axios';
-import { Request, Response } from 'express';
-
-export async function getFavoriteSongs(req: Request, res: Response) {
-  if (req.isAuthenticated()) {
-    const user = req.user as any;
-    const accessToken = user.accessToken;
-
-    try {
-      const response = await axios.get('https://api.spotify.com/v1/me/top/tracks', {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-
-      const favoriteSongs = response.data.items;
-
-      // Aquí puedes realizar acciones adicionales con las canciones favoritas, como almacenarlas en la base de datos
-
-      res.json(favoriteSongs);
-    } catch (error) {
-      console.error('Error al obtener las canciones favoritas:', error);
-      res.status(500).json({ error: 'Error al obtener las canciones favoritas' });
-    }
-  } else {
-    res.status(401).json({ error: 'No autenticado' });
-  }
-}

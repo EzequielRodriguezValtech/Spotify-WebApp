d5bc078bd451092cb61ab471ba5944f517b4f8ad
EzequielRodriguezValtech <ezequiel.rodriguez@valtech.com>
2023-06-09 10:30:38 -0300

    front changes

--- Front/spotifywebapp/src/app/page.tsx
+++ Front/spotifywebapp/src/app/page.tsx
@@ -1,15 +1,17 @@
 // Imports
 import Image from 'next/image';
-import Head from 'next/head'
+import Head from 'next/head';
 // Components
-
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
     <Head>
-      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-    </Head><main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <link rel="shortcut icon" href="images/icon_spotify.png" type="image/x-icon" />
+    </Head>
+    <Link href="../components/spotifyHeader">Header</Link>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
             Get started by editing: &nbsp;
@@ -24,7 +26,7 @@ export default function Home() {
             >
               By{' '}
               <Image
-                src="/vercel.svg"
+                src="images/vercel.svg"
                 alt="Vercel Logo"
                 className="dark:invert"
                 width={100}
@@ -34,7 +36,7 @@ export default function Home() {
           </div>
           <div>
             <Image
-              src="/next.svg"
+              src="images/next.svg"
               alt="Next Logo"
               className="dark:invert"
               width={100}
@@ -43,6 +45,7 @@ export default function Home() {
           </div>
           <div className=''>
             <h1>SPOTIFY WEBAPP</h1>
+            <a href="http://localhost:3000/login" target="_blank" rel="noopener noreferrer">Login</a>
           </div>
         </div>
       </main>
